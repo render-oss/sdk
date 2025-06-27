2e6c3726563c4a0bf47277c49779813c31378d5e
Mark Benjamin <mark@render.com>
2025-06-27 10:37:23 -0400

    Add error log

--- go/pkg/internal/server/server.go
+++ go/pkg/internal/server/server.go
@@ -39,6 +39,7 @@ func (h *ServerHandler) PostStart(ctx context.Context, request PostStartRequestO
 
 	err := h.serverOrchestrator.StartTask(request.Body.ResponseUrl, request.Body.Name, request.Body.TaskId, input...)
 	if err != nil {
+		log.Printf("Error starting task: %v", err)
 		return PostStart400Response{}, err
 	}
 
