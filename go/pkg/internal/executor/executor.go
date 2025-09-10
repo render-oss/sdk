2e7a540f8f5b6be0510ecd50f767ae80d049e813
Mark Benjamin <mark@render.com>
2025-09-10 16:09:26 -0400

    remove server and adapter

--- go/pkg/internal/executor/executor.go
+++ go/pkg/internal/executor/executor.go
@@ -2,49 +2,84 @@ package executor
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"runtime/debug"
 
+	"github.com/renderinc/workflow-sdk/go/pkg/internal/callbackapi"
 	"github.com/renderinc/workflow-sdk/go/pkg/internal/task"
 )
 
 type Executor struct {
-	tasks *task.Tasks
+	tasks      *task.Tasks
+	callbacker *callbackapi.ClientWithResponses
 }
 
 type CompleteTask func(ctx context.Context, taskName string, result []interface{}, err error) error
 type ExecuteTask func(taskName string, input ...interface{}) ([]interface{}, error)
 
-func NewExecutor(tasks *task.Tasks) *Executor {
+func NewExecutor(tasks *task.Tasks, callbacker *callbackapi.ClientWithResponses) *Executor {
 	return &Executor{
-		tasks: tasks,
+		tasks:      tasks,
+		callbacker: callbacker,
 	}
 }
 
-func (e *Executor) Execute(ctx context.Context, completeTask CompleteTask, executeTask ExecuteTask, taskName string, input ...interface{}) error {
+func (e *Executor) Execute(ctx context.Context, taskName string, input ...interface{}) error {
 	// Ensure the task is registered
 	_, err := e.tasks.GetTaskByName(taskName)
 	if err != nil {
 		return err
 	}
 
-	go func() {
-		defer func() {
-			if r := recover(); r != nil {
-				log.Printf("Recovered from panic: %v", r)
-				log.Printf("Stack trace: %s", string(debug.Stack()))
-			}
-		}()
+	// TODO: Update this once we have sub tasks working
+	noOpExecutorContext := newExecutorContext(func(taskName string, input ...interface{}) ([]interface{}, error) {
+		return nil, nil
+	})
 
-		log.Printf("Executing task: %s, input: %v", taskName, input)
+	result, err := e.tasks.ExecuteTaskByName(taskName, noOpExecutorContext, input...)
 
-		result, err := e.tasks.ExecuteTaskByName(taskName, newExecutorContext(executeTask), input...)
-		log.Printf("Task completed: %s", taskName)
-		taskErr := completeTask(context.Background(), taskName, result, err)
-		if taskErr != nil {
-			log.Printf("Error completing task: %s", taskErr)
+	return e.completeTask(context.Background(), taskName, result, err)
+}
+
+func (e *Executor) completeTask(ctx context.Context, taskName string, result []interface{}, taskErr error) error {
+	output, err := json.Marshal(result)
+	if err != nil {
+		return fmt.Errorf("failed to marshal output: %w", err)
+	}
+
+	var callbackRequest *callbackapi.CallbackRequest
+
+	if taskErr != nil {
+		isReportedBySdk := true
+		callbackRequest = &callbackapi.CallbackRequest{
+			Status: callbackapi.Error,
+			Error: &callbackapi.TaskError{
+				Details:         taskErr.Error(),
+				IsReportedBySdk: &isReportedBySdk,
+			},
 		}
-	}()
+	} else {
+		callbackRequest = &callbackapi.CallbackRequest{
+			Status: callbackapi.Complete,
+			Complete: &callbackapi.TaskComplete{
+				Output: output,
+			},
+		}
+	}
+
+	callbackRequest.Metadata = &callbackapi.TaskMetadata{
+		TaskName: taskName,
+	}
+
+	resp, err := e.callbacker.PostCallbackWithResponse(ctx, *callbackRequest)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("callback failed with status code %d", resp.StatusCode())
+	}
 
 	return nil
 }
--- go/pkg/internal/executor/executor_test.go
+++ go/pkg/internal/executor/executor_test.go
@@ -2,10 +2,15 @@ package executor_test
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
+	"github.com/renderinc/workflow-sdk/go/pkg/internal/callbackapi"
 	"github.com/renderinc/workflow-sdk/go/pkg/internal/executor"
 	"github.com/renderinc/workflow-sdk/go/pkg/internal/task"
 	"github.com/stretchr/testify/require"
@@ -19,118 +24,71 @@ func failingTask(_ task.TaskContext) (string, error) {
 	return "partial result", errors.New("task failed")
 }
 
-// parentTask is defined up here so we can get a real name for the task
-// anonymous functions don't have a clear name
-type parentTask struct {
-	t *testing.T
-}
-
-func (p *parentTask) ParentTask(ctx task.TaskContext) (interface{}, error) {
-	result := ctx.ExecuteTask(testTask)
-	require.NoError(p.t, result.Error)
-	return "parent-" + result.Result[0].(string), nil
-}
-
 func TestExecuteTask(t *testing.T) {
 	t.Run("simple task", func(t *testing.T) {
 		tasks := task.NewTasks()
 		err := tasks.RegisterTask(testTask)
 		require.NoError(t, err)
 
-		completeTaskCalled := false
-		completeTask := func(ctx context.Context, taskName string, result []interface{}, err error) error {
-			completeTaskCalled = true
-			require.Equal(t, taskName, "testTask")
-			require.NoError(t, err)
-			resultValue := result[0].(string)
-			require.Equal(t, resultValue, "test")
-			return nil
-		}
-		executeTask := func(taskName string, input ...interface{}) ([]interface{}, error) {
-			require.Fail(t, "should not be called")
-			return []interface{}{"test"}, nil
-		}
+		callbackCalled := false
 
-		executor := executor.NewExecutor(tasks)
-		err = executor.Execute(context.Background(), completeTask, executeTask, "testTask")
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, r.URL.Path, "/callback")
+			body, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
+
+			var callbackRequest callbackapi.CallbackRequest
+			err = json.Unmarshal(body, &callbackRequest)
+			require.NoError(t, err)
+
+			require.Equal(t, callbackRequest.Complete.Output, []byte("[\"test\"]"))
+			callbackCalled = true
+			w.WriteHeader(200)
+		}))
+
+		callbackerClient, err := callbackapi.NewClientWithResponses(srv.URL)
+		require.NoError(t, err)
+
+		executor := executor.NewExecutor(tasks, callbackerClient)
+		err = executor.Execute(context.Background(), "testTask")
 		require.NoError(t, err)
 
 		require.Eventually(t, func() bool {
-			return completeTaskCalled
-		}, time.Second*1, time.Millisecond*100)
-	})
-
-	t.Run("subtask", func(t *testing.T) {
-		tasks := task.NewTasks()
-		err := tasks.RegisterTask(testTask)
-		require.NoError(t, err)
-
-		executor := executor.NewExecutor(tasks)
-
-		completeTaskCalled := false
-		completeTask := func(ctx context.Context, taskName string, result []interface{}, err error) error {
-			completeTaskCalled = true
-			require.NoError(t, err)
-			resultValue := result[0].(string)
-			require.Equal(t, "parent-subtask", resultValue)
-			return nil
-		}
-		executeTask := func(taskName string, input ...interface{}) ([]interface{}, error) {
-			require.Equal(t, taskName, "testTask")
-			require.Len(t, input, 0)
-			return []interface{}{"subtask"}, nil
-		}
-
-		p := parentTask{t: t}
-		err = tasks.RegisterTask(p.ParentTask)
-		require.NoError(t, err)
-
-		err = executor.Execute(context.Background(), completeTask, executeTask, "ParentTask")
-		require.NoError(t, err)
-
-		require.Eventually(t, func() bool {
-			return completeTaskCalled
+			return callbackCalled
 		}, time.Second*1, time.Millisecond*100)
 	})
 
 	t.Run("task with error", func(t *testing.T) {
-		// Test that errors from tasks are properly passed through
 		tasks := task.NewTasks()
 		err := tasks.RegisterTask(failingTask)
 		require.NoError(t, err)
 
-		completeTaskCalled := false
-		var receivedError error
-		var receivedResult []interface{}
+		callbackCalled := false
 
-		completeTask := func(ctx context.Context, taskName string, result []interface{}, err error) error {
-			completeTaskCalled = true
-			receivedError = err
-			receivedResult = result
-			require.Equal(t, "failingTask", taskName)
-			return nil
-		}
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, r.URL.Path, "/callback")
+			body, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
 
-		executeTask := func(taskName string, input ...interface{}) ([]interface{}, error) {
-			require.Fail(t, "should not be called")
-			return nil, nil
-		}
+			var callbackRequest callbackapi.CallbackRequest
+			err = json.Unmarshal(body, &callbackRequest)
+			require.NoError(t, err)
 
-		executor := executor.NewExecutor(tasks)
-		err = executor.Execute(context.Background(), completeTask, executeTask, "failingTask")
+			require.Equal(t, callbackRequest.Error.Details, "task failed")
+			require.True(t, *callbackRequest.Error.IsReportedBySdk)
+			callbackCalled = true
+			w.WriteHeader(200)
+		}))
+
+		callbackerClient, err := callbackapi.NewClientWithResponses(srv.URL)
+		require.NoError(t, err)
+
+		executor := executor.NewExecutor(tasks, callbackerClient)
+		err = executor.Execute(context.Background(), "failingTask")
 		require.NoError(t, err)
 
 		require.Eventually(t, func() bool {
-			return completeTaskCalled
+			return callbackCalled
 		}, time.Second*1, time.Millisecond*100)
-
-		// Verify the error was passed through
-		require.Error(t, receivedError)
-		require.Equal(t, "task failed", receivedError.Error())
-
-		// Verify the result was passed through (without the error)
-		require.NotNil(t, receivedResult)
-		resultSlice := receivedResult[0].(string)
-		require.Equal(t, "partial result", resultSlice)
 	})
 }
