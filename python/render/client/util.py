2d7941e11850fdc63a64ed3f0aab5014c072e7ed
Mark Benjamin <mark@render.com>
2025-09-29 15:39:47 -0400

    Fix lint

--- python/render/client/util.py
+++ python/render/client/util.py
@@ -172,7 +172,7 @@ def handle_http_errors(operation: str):
                 raise
             except Exception as exc:
                 # Unexpected exception
-                raise RenderError(f"{operation} failed with unexpected error: {exc}")
+                raise RenderError(f"{operation} failed with unexpected error: {exc}") from exc
 
         return wrapper
 
