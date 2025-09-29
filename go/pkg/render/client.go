c8dd740737d10a6ac70f703db39bffb49553750b
Mark Benjamin <mark@render.com>
2025-09-29 10:36:11 -0400

    Allow env var override for local server

--- go/pkg/render/client.go
+++ go/pkg/render/client.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/renderinc/workflow-sdk/go/pkg/render/internal/client"
+)
+
+const (
+	LocalDevURL = "RENDER_LOCAL_DEV_URL"
+	UseLocalDev = "RENDER_USE_LOCAL_DEV"
 )
 
 // Client represents the Render API client
@@ -50,6 +56,16 @@ func NewClient(token string, options ...ClientOption) (*Client, error) {
 	// Ensure base URL has protocol
 	if !strings.HasPrefix(baseURL, "http://") && !strings.HasPrefix(baseURL, "https://") {
 		baseURL = "https://" + baseURL
+	}
+
+	useLocalDev := os.Getenv(UseLocalDev)
+	if useLocalDev != "" {
+		baseURL = "http://localhost:8120"
+	}
+
+	localDevURL := os.Getenv(LocalDevURL)
+	if localDevURL != "" {
+		baseURL = localDevURL
 	}
 
 	// Create the internal generated client with auth
