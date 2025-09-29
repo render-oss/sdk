3046594d241dfeec7afab303742a0fe1452c76d6
Mark Benjamin <mark@render.com>
2025-09-29 15:43:17 -0400

    Fix format

--- python/render/client/util.py
+++ python/render/client/util.py
@@ -172,7 +172,9 @@ def handle_http_errors(operation: str):
                 raise
             except Exception as exc:
                 # Unexpected exception
-                raise RenderError(f"{operation} failed with unexpected error: {exc}") from exc
+                raise RenderError(
+                    f"{operation} failed with unexpected error: {exc}"
+                ) from exc
 
         return wrapper
 
